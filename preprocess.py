59005262effbf0c2707beb9b16a22a5249db1fd3
nerdlab53 <retr0sushi.04@gmail.com>
2024-02-17 22:33:29 +0530

    modify preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,15 @@
 import opendatasets as od
 import pandas as pd
 import json
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-fn", "--filename", help='name of the output json file')
+
+args = parser.parse_args()
+
+filename = args.filename
 
 # username : himanshubhenwal key : c729846b22a3daccd9c055a05a5dc263
 
@@ -8,12 +17,11 @@ od.download('https://www.kaggle.com/datasets/cid007/mental-disorder-classificati
 
 df = pd.read_csv('./mental-disorder-classification/Dataset-Mental-Disorders.csv')
 
+df.rename(columns = {'Sleep dissorder': 'Sleep Disorder', 'Anorxia' : 'Anorexia', 'Optimisim' : 'Optimism'}, inplace=True)
+
 # separating features and labels
 labels = df[['Expert Diagnose']]
-features = df[['Sadness', 'Euphoric', 'Sleep dissorder', 'Suicidal thoughts', 'Anorxia', 'Aggressive Response', 'Ignore & Move-On', 'Nervous Break-down', 'Admit Mistakes', 'Overthinking', 'Sexual Activity', 'Concentration', 'Optimisim']]
-
-# renaming some badly spelt names in the dataset
-features.rename(columns = {'Sleep dissorder': 'Sleep Disorder', 'Anorxia' : 'Anorexia', 'Optimisim' : 'Optimism'}, inplace=True)
+features = df[['Sadness', 'Euphoric', 'Sleep Disorder', 'Suicidal thoughts', 'Anorexia', 'Aggressive Response', 'Ignore & Move-On', 'Nervous Break-down', 'Admit Mistakes', 'Overthinking', 'Sexual Activity', 'Concentration', 'Optimism']]
 
 
 # definig the json template
@@ -40,19 +48,35 @@ json_template = {
     }
 }
 
-feature_names = list(features.columns)
+def write_json(features, labels, json_template, filename):
+  '''
+  Takes a dataframe and the JSON template as inputs and generates dataset
+
+  Args : 
+  
+  features : Pandas DataFrame Object Containing all the features
+
+  labels : Pandas DataFrame Object Containing all the labels
+
+  json_template : A JSON template
+
+  Outputs : 
+
+  Writes to a <name>.json
+  '''
+  feature_names = list(features.columns)
 
 # iterating over the dataframe and populate the JSON template
-json_output = []
-for row in features.iterrows():
-    json_object = json_template.copy()
-    for i in feature_names:
-      json_object['input'][str(i)] = row[1][str(i)]
-for row in labels.iterrows():
-    json_object['output'] = row[1]['Expert Diagnose']
-    json_output.append(json_object)
+  json_output = []
+  for row in df.iterrows():
+      json_object = json_template.copy()
+      for i in feature_names:
+        json_object['input'][str(i)] = row[1][str(i)]
+      for row in labels.iterrows():
+          json_object['output'] = row[1]['Expert Diagnose']
+      json_output.append(json_object)
 
 
-# writing the JSON output to a file
-with open('output_final.json', 'w') as f:
-    json.dump(json_output, f)
+  # writing the JSON output to a file
+  with open(f'{filename}.json', 'w') as f:
+      json.dump(json_output, f, indent=4)
